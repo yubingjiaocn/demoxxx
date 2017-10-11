f6493db79b6b7b29a0985e3e135572556260b317
BingJiao Yu <yubj@cn.ibm.com>
2017-10-11 17:06:26 +0800

    Changed Code

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -9,19 +9,10 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import java.util.ArrayList;
 
-
-@Path("v1/example")
+@Path("hello")
 public class Example {
-
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response example() {
-        List<String> list = new ArrayList<>();
-        //return a simple list of strings
-        list.add("Some data");
-        return Response.ok(list.toString()).build();
-    }
-
-
+  @GET
+  public String greet() throws Exception {
+    return "Hello from " + java.net.InetAddress.getLocalHost().getHostName();
+  }	
 }
